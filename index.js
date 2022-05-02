08391e4d11d076bba6692597c857b8b6cf9c82ee
hector <hectorlopezp1998@gmail.com>
2022-05-02 18:52:18 -0400

    commit tree 93b17ea1576b9764e46ae5024d0ef56726c3d8ec

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((resp) => { return resp.json() })
+    // .then((json) => console.log(json))
+    .then((data) => { return renderBooks(data) })
 }
 
 function renderBooks(books) {
@@ -12,6 +15,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
